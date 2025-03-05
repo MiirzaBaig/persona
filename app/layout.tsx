5fda26ee64a227ac316e602bfbb56b56f16122f1
Mirza Ismail Baig <mirza.devs@gmail.com>
2025-03-05 10:59:10 +0530

    Modify

---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import Footer from "@/components/footer";
+import Nav from "@/components/nav";
+import { Analytics } from "@vercel/analytics/react"
+import ogImage from "./opengraph-image.png";
+
+export const metadata: Metadata = {
+  title: "Mirza Baig",
+  description: "software engineer",
+  metadataBase: new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`),
+  openGraph: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height
+      },
+    ],
+  },
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${GeistSans.variable} ${GeistMono.variable} relative 82d51828-c8fb-46bb-9edd-4d6a5378b4e4 font-sans bg-whiteout selection:text-white 24576e23-44f4-4174-a049-9a0ebd609e65 selection:bg-pink-400 dark:bg-zinc-900 text-blackout dark:text-zinc-100`}
+      >
+        <ThemeProvider attribute="class">
+          <Nav/>
+          {children}
+          <Footer />
+        </ThemeProvider>
+        <Analytics />
+      </body>
+    </html>
+  );
+}
+
+export const revalidate = 300;
---
+++ components/footer.tsx
@@ -0,0 +1,184 @@
+import Link from "next/link";
+import ThemeToggle from "./theme-toggle";
+
+interface FooterProps {}
+
+const Footer = ({}: FooterProps) => {
+  return (
+    <footer className="flex justify-center w-full flex-col items-center ">
+      <div className="fixed bottom-0 w-full z-50">
+        <svg
+          className="hidden md:block absolute bottom-0 right-0 rotate-180 z-50 md:absolute "
+          width="53"
+          height="52"
+          viewBox="0 0 53 52"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            className="dark:fill-black fill-blackout"
+            d="M0 -5.35442e-05H52.1C52.1 -5.35442e-05 24.8947 -0.434101 12.4473 12.7519C2.67029e-05 25.938 0 51.8759 0 51.8759V-5.35442e-05Z"
+          />
+        </svg>
+
+        <svg
+          className="hidden md:block absolute left-0 bottom-0 -scale-x-100 rotate-180 z-50"
+          width="53"
+          height="52"
+          viewBox="0 0 53 52"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            className="dark:fill-black fill-blackout"
+            d="M0 -5.35442e-05H52.1C52.1 -5.35442e-05 24.8947 -0.434101 12.4473 12.7519C2.67029e-05 25.938 0 51.8759 0 51.8759V-5.35442e-05Z"
+          />
+        </svg>
+      </div>
+      <div className="bg-blackout p-20 pb-10 rounded-2xl mb-4 md:mb-20 text-whiteout flex flex-col gap-20 md:gap-0 items-center md:items-start md:grid md:grid-cols-4">
+        <div className="w-[200px] flex flex-col items-center text-center md:items-start md:text-start">
+          <h2 className="text-lg font-semibold">Find me on</h2>
+          <ul className="font-light gap-1 flex flex-col mt-2">
+          <li>
+              <Link
+                className="transition-opacity opacity-75 hover:opacity-100"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://twitter.com/MiirzaBaig"
+              >
+                x.com
+              </Link>
+            </li>
+            <li>
+            </li>
+            <li>
+              <Link
+                className="transition-opacity opacity-75 hover:opacity-100"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://www.linkedin.com/in/mirza-baig-590b1826b/"
+              >
+                LinkedIn
+              </Link>
+            </li>
+           
+            <li>
+              <Link
+                className="transition-opacity opacity-75 hover:opacity-100"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/MiirzaBaig"
+              >
+                GitHub
+              </Link>
+            </li>
+            <li>
+            </li>
+            <li>
+            </li>
+           
+          </ul>
+        </div>
+
+        <div className="col-span-2 flex justify-end text-center md:items-start md:text-start">
+          <div className="flex flex-col">
+            <h2 className="text-lg font-semibold">Contact</h2>
+            <div className=" font-light mt-2">
+              <Link
+                className="opacity-50 transition-opacity hover:opacity-100"
+                href="mailto:parzerphilipp@gmail.com"
+              >
+                mirza.devs@gmail.com
+              </Link>
+              <p>
+                <Link
+                  className="opacity-50 transition-opacity hover:opacity-100"
+                  href="tel:+919121776155"
+                >
+                  +919121776155
+                </Link>
+              </p>
+              <Link
+                rel="noopener noreferrer"
+                className="opacity-50 transition-opacity hover:opacity-100"
+                target="_blank"
+                href=""
+              >
+                <p className="mt-2">Hyderabad</p>
+                <p>India</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-4 md:mt-20 text-center md:items-start md:text-start md:-order-none order-first">
+          <h2 className="font-semibold mb-2">Theme</h2>
+          <div>
+            <ThemeToggle />
+          </div>
+        </div>
+
+        <div className="col-span-4 md:mt-10 text-sm text-center md:text-start">
+          <div className="text-whiteout/50">
+            this site is{" "}
+            <Link
+              className="underline decoration-2 text-whiteout/75 hover:text-whiteout/100"
+              href="https://github.com/philparzer/website-v3"
+            >
+              open source
+            </Link>
+            <p>
+              built with some of my favorite tools &mdash;{" "}
+              <Link
+                className="underline decoration-2 text-whiteout/75 hover:text-whiteout/100"
+                href="https://nextjs.org"
+                target="_blank"
+              >
+                next.js
+              </Link>
+              {", "}
+              <Link
+                className="underline decoration-2 text-whiteout/75 hover:text-whiteout/100"
+                href="https://tailwindcss.com"
+                target="_blank"
+              >
+                tailwind
+              </Link>
+              {", "}
+              <Link
+                className="underline decoration-2 text-whiteout/75 hover:text-whiteout/100"
+                href="https://framer.com/motion"
+                target="_blank"
+              >
+                framer motion
+              </Link>
+              {", "}and{" "}
+              <Link
+                className="underline decoration-2 text-whiteout/75 hover:text-whiteout/100"
+                href="https://rive.app"
+                target="_blank"
+              >
+                rive
+              </Link>
+            </p>
+          </div>
+          <div className="flex justify-between flex-wrap items-baseline text-center md:text-start">
+            <p className="text-whiteout/50 md:w-auto w-full">
+              if you have questions or inquiries,{" "}
+              <Link
+                href="mailto:mirza.devs@gmail.com"
+                className="underline decoration-2 text-whiteout/75 hover:text-whiteout/100"
+              >
+                reach out
+              </Link>
+            </p>
+            <p className="text-xs opacity-50 text-center w-full mt-10 md:mt-0 md:w-auto">
+              last update: WINTER2025
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
